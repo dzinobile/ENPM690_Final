4944193dc83816ef273269ae296b0f8e2c4571f5
barskeee <ebarski@umd.edu>
2026-04-28 14:07:24 -0400

    Altered mesh files to make them compatible with import into mujoco simulation space. Fixed comment typo on .xml files. Currently working on adding working joints to tars_meshed.

---
+++ simulation_practice/tars_meshed_sim.py
@@ -0,0 +1,14 @@
+import mujoco
+import mujoco.viewer
+import time
+
+model = mujoco.MjModel.from_xml_path("tars_meshed.xml")
+data = mujoco.MjData(model)
+
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    while viewer.is_running():
+        step_start = time.time()
+        mujoco.mj_step(model, data)
+        viewer.sync()
+        elapsed = time.time() - step_start
+        time.sleep(max(0, model.opt.timestep - elapsed))
